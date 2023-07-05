aa354e4f9f0bb80a7fbbc375b85fa74afcbd0e98
Musa Gillani <musagillaniwork@gmail.com>
2023-07-05 20:15:00 +0500

    fix colors for toggle in light and dark themes

--- components/ui/toggle.tsx
+++ components/ui/toggle.tsx
@@ -1,19 +1,19 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as TogglePrimitive from "@radix-ui/react-toggle"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import * as TogglePrimitive from "@radix-ui/react-toggle";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const toggleVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-white transition-colors hover:bg-slate-100 hover:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-400 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-slate-100 data-[state=on]:text-slate-900 dark:ring-offset-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-400 dark:focus-visible:ring-slate-800 dark:data-[state=on]:bg-slate-800 dark:data-[state=on]:text-slate-50",
+  "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-white transition-colors hover:bg-secondary-100 hover:text-secondary-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-secondary-400 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-secondary-800 data-[state=on]:text-secondary-100 dark:ring-offset-secondary-950 dark:hover:bg-secondary-800 dark:hover:text-secondary-400 dark:focus-visible:ring-secondary-800 dark:data-[state=on]:bg-secondary-800 dark:data-[state=on]:text-secondary-50",
   {
     variants: {
       variant: {
         default: "bg-transparent",
         outline:
-          "border border-slate-200 bg-transparent hover:bg-slate-100 hover:text-slate-900 dark:border-slate-800 dark:hover:bg-slate-800 dark:hover:text-slate-50",
+          "border border-secondary-200 bg-transparent hover:bg-slate-100 hover:text-secondary-900 dark:border-secondary-800 dark:hover:bg-secondary-800 dark:hover:text-secondary-50",
       },
       size: {
         default: "h-10 px-3",
@@ -26,7 +26,7 @@ const toggleVariants = cva(
       size: "default",
     },
   }
-)
+);
 
 const Toggle = React.forwardRef<
   React.ElementRef<typeof TogglePrimitive.Root>,
@@ -38,8 +38,8 @@ const Toggle = React.forwardRef<
     className={cn(toggleVariants({ variant, size, className }))}
     {...props}
   />
-))
+));
 
-Toggle.displayName = TogglePrimitive.Root.displayName
+Toggle.displayName = TogglePrimitive.Root.displayName;
 
-export { Toggle, toggleVariants }
+export { Toggle, toggleVariants };
