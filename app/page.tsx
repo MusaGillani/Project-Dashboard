6672fd9f47b9a58691f26d10595e2de87c46c99e
Musa Gillani <musagillaniwork@gmail.com>
2023-07-05 20:19:58 +0500

    redirecting authenticated users to dash

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 import { Metadata } from "next";
+import { redirect } from "next/navigation";
+
 import { UserAuthForm } from "@/components/user-auth-form";
 
 export const metadata: Metadata = {
@@ -6,7 +10,17 @@ export const metadata: Metadata = {
   description: "User login",
 };
 
-export default function AuthenticationPage() {
+export default async function AuthenticationPage() {
+  const supabase = createServerComponentClient({ cookies });
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user) {
+    // Authenticated users will be redirected to the `/dash` route.
+    redirect("/dash");
+  }
   return (
     <div className="flex h-full min-h-screen flex-col justify-center bg-white dark:bg-black">
       <UserAuthForm />
